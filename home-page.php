29d7ccb1b5d56d2c78a5a7f1ad335376755825f1
Hari om singh <131800481+hariomsingh2004@users.noreply.github.com>
2024-01-27 19:30:53 +0530

    Add files via upload

---
+++ home-page.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<title>
+Home
+</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="navbar.css" />
+<link rel="stylesheet" href="homepage.css">
+<style>
+	.thead-dark {
+		background-color: #000;
+		color: #fff;
+	}
+	body{
+	/* background-image: url("bg.jpg"); */
+	background: #7FFFD4;
+	}
+</style>
+</head>
+<body>
+<nav class="navbar navbar-expand-xl navbar-dark bg-dark">
+<a class="navbar-brand" href="#">College</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample06"
+aria-controls="navbarsExample06" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarsExample06">
+<ul class="navbar-nav mr-auto">
+<li class="nav-item">
+<a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="department-display.php">Departments</a>
+</li>
+<li class="nav-item">
+<a class="nav-link" href="course-display.php">Courses</a>
+</li>
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown06" data-toggle="dropdown"
+aria-haspopup="true" aria-expanded="false">People</a>
+<div class="dropdown-menu" aria-labelledby="dropdown06">
+<a class="dropdown-item" href="student-signin.html">Student</a>
+<a class="dropdown-item" href="teacher-signin.html">Faculty</a>
+<a class="dropdown-item" href="hod-signin.html">HOD</a>
+</div>
+</li>
+</ul>
+</div>
+</nav>
+</head>
+<body>
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <title>Online College Portal</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <section class="about-us">
+    <div class="about">
+      <img src="bg.jpg" class="pic">
+      <div class="text">
+        <h4>WELCOME TO </h4>
+        <h2>ONLINE COLLEGE <span> PORTAL</span></h2>
+          <p>Bansal Institute of Engineering & Technology is managed by Bansal Educational Trust founded in the year 2007 and registered under the Societies Registration Act of 1860 with an aim of providing quality technical education. The Institute is affiliated to AKTU, Lucknow and approved by AICTE,.Ministry of HRD, Govt of India.
+The Institute is located on the main 120ft. NH-24, Sitapur road and is situated at a distance of only 1 Kms from AKTU and behind the wall known Sewa Hospital and the Brij Ki Rasoi. Thus, being away from the crowd of the city the college is situated in a spacious lush green campus having natural environment conducive for well planned studies……</p>
+        <div class="data">
+        <a href="https://www.bansaliet.in/" class="hire">show More </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
+</body>
+</html>
